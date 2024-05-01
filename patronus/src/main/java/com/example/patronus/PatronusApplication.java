b28bc1b6de6d36fba2169fa39ec552cbe07ab022
narminya <narminya@code.edu.az>
2024-05-01 20:59:50 +0400

    Initial commit

---
+++ patronus/src/main/java/com/example/patronus/PatronusApplication.java
@@ -0,0 +1,13 @@
+package com.example.patronus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PatronusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PatronusApplication.class, args);
+    }
+
+}
---
+++ patronus/src/test/java/com/example/patronus/PatronusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.patronus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PatronusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
